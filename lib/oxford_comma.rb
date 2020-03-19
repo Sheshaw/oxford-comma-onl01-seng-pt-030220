7d5232b37d1292d499822ebe08ee5bf780823a06
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-19 23:54:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,3 @@
 def oxford_comma(str)
-  new_str  = 
-  if str.size == 2 
-    new_str << str.join(" and ")
-    
-  elsif str.size == 1
-    str.join(",")
-  end
   
-  puts new_str
-  new_str
 end
