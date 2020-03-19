61c30ccc0495e20b524831fd103492ee4abae93b
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-19 23:50:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,8 @@ def oxford_comma(array)
   new_array  = []
   if array.size == 2 
     new_array << array.join(" and ")
-  elsif
-    array.size == 1
-    array
+  elsif array.size == 1
+    array.join
   end
   puts new_array
   new_array
