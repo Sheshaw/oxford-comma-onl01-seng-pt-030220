df964a24774676fde1567960c07d9d5e4fc7731c
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-20 00:24:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,10 @@ def oxford_comma(array)
     array.join(",")
   elsif array.size == 2
     array.join(" and ")
+  elsif array.size == 3
+  new_array = []
+  new_array << array.join(",")
+  new_array.join[3](",")
   else
    
  end
