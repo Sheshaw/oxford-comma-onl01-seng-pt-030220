9fd46c72aa5b169294020f0068b0215fbb899856
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-20 00:46:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,11 @@
 def oxford_comma(array)
-  if array.size == 0
-    array.join(",")
-    
-  elsif array.size == 1
-    array.join(" and ")
-    
-  elsif array.size > 2
-     array[-2] << " , and "
-     array.join
-  else
-
- end
+  if array.length == 2
+    array[-2] << " and "
+    array.join
+  elsif array.length == 1
+    array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+    array.join(", ")
+  end
 end
