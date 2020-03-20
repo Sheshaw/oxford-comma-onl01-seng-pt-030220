b982b2406af2230e2f1ccf95705ed9e25ab1e175
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-20 00:56:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
+  
   if array.size == 2
-    
     array[-2] << " and "
     array.join
     
@@ -8,7 +8,7 @@ def oxford_comma(array)
     array.join
     
   elsif array.size > 2
-    array[-1].prepend "and " #the prepend method will add o #the element
+    array[-1].prepend "and " #the prepend method will add to                                  #the element
     array.join(", ")
   end
 end
