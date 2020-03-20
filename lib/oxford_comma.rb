e39a7c01411211a249ea62c552f798c3a26bf5da
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-20 00:46:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,14 @@
 def oxford_comma(array)
-  if array.size == 1
+  if array.size == 0
     array.join(",")
     
-  elsif array.size == 2
+  elsif array.size == 1
     array.join(" and ")
     
-  elsif array.size == 3
-  new_array = []
-  new_array << array.join(",")
-  new_array.
-  new_array
+  elsif array.size > 2
+     array[-2] << " , and "
+     array.join
   else
+
  end
 end
